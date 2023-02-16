0e65d52ff97d484bff6e17724c7c5e69f4644385
Andrei Litvin <andy314@gmail.com>
2023-02-16 10:40:55 -0500

    Compilable main with using the new QR code logic

--- examples/common/imgui_ui/ui.h
+++ examples/common/imgui_ui/ui.h
@@ -14,6 +14,8 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+#pragma once
+
 #include "ui_window.h"
 
 #include <AppMain.h>
