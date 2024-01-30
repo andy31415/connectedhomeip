19b4937159469ba5c279ade0b44199f664a0fca5
Andrei Litvin <andreilitvin@google.com>
2024-01-30 12:38:49 -0500

    Fix pragma once. At least now I get ember include errors

--- src/attributes/ember/interface.cpp
+++ src/attributes/ember/interface.cpp
@@ -15,8 +15,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-#pragma once
-
 #include <attributes/ember/interface.h>
 #include <lib/support/CodeUtils.h>
 
