13eeaedc2e45e6ec4548b63383c582ec948c085c
Andrei Litvin <andreilitvin@google.com>
2024-01-30 12:35:23 -0500

    Add a build script - did not integrate with build_examples since I assume I will delete the example

---
+++ examples/attribute_database/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -ex
+
+CHIP_ROOT=$(readlink -f $(readlink -f $(dirname ${BASH_SOURCE[0]}))/../..)
+
+gn gen --check --fail-on-unused-args --export-compile-commands --root=$CHIP_ROOT/examples/attribute_database/ $CHIP_ROOT/out/linux-x64-attribute-database
+ninja -C $CHIP_ROOT/out/linux-x64-attribute-database
