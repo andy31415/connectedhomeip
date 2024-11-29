0cc66dd006e09fcdfc0f9901f718698492d12717
Andrei Litvin <andy314@gmail.com>
2024-11-29 12:11:03 -0500

    Fix an include

--- src/app/code-data-model-provider/tests/TestMetadata.cpp
+++ src/app/code-data-model-provider/tests/TestMetadata.cpp
@@ -14,10 +14,10 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-#include "access/Privilege.h"
 #include <optional>
 #include <pw_unit_test/framework.h>
 
+#include <access/Privilege.h>
 #include <app/code-data-model-provider/Metadata.h>
 
 using namespace chip::Access;
