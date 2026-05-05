aa51245ef246d937515f0324b0c58c05df2cc9cc
Andrei Litvin <andy314@gmail.com>
2026-05-05 11:28:25 -0400

    Restore truncated license

--- examples/all-devices-app/all-devices-common/devices/device-factory/DeviceFactory.h
+++ examples/all-devices-app/all-devices-common/devices/device-factory/DeviceFactory.h
@@ -2,6 +2,7 @@
  *    Copyright (c) 2026 Project CHIP Authors
  *    All rights reserved.
  *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
  *
